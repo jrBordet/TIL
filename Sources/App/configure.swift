aeb9978095dc5078aec81830c80017bf104a006a
Jean Raphael Bordet <jr.bordet@touchwa.re>
2018-11-26 12:18:05 +0100

    Initial commit

---
+++ Sources/App/configure.swift
@@ -0,0 +1,48 @@
+// 1
+import FluentPostgreSQL
+import Vapor
+
+/// Called before your application initializes.
+///
+/// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#configureswift)
+public func configure(
+    _ config: inout Config,
+    _ env: inout Environment,
+    _ services: inout Services
+    ) throws {
+    // Register providers first
+    try services.register(FluentPostgreSQLProvider())
+    
+    // Register routes to the router
+    let router = EngineRouter.default()
+    try routes(router)
+    services.register(router, as: Router.self)
+    
+    // Register middleware
+    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+    // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    services.register(middlewares)
+    
+    // Configure a database
+    var databases = DatabasesConfig()
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+    let databaseConfig = PostgreSQLDatabaseConfig(
+        hostname: hostname,
+        username: username,
+        database: databaseName,
+        password: password)
+    let database = PostgreSQLDatabase(config: databaseConfig)
+    databases.add(database: database, as: .psql)
+    services.register(databases)
+    
+    // Configure migrations
+    var migrations = MigrationConfig()
+    migrations.add(model: Acronym.self, database: .psql)
+    services.register(migrations)
+    
+    // Configure the rest of your application here
+}
---
+++ Sources/App/routes.swift
@@ -0,0 +1,52 @@
+import Vapor
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+    // Basic "It works" example
+    router.get { req in
+        return "It works!"
+    }
+    
+    // Basic "Hello, world!" example
+    router.get("hello") { req in
+        return "Hello, world!"
+    }
+    
+    router.post("api", "acronyms") { (req: Request) -> Future<Acronym> in
+        return try req
+            .content
+            .decode(Acronym.self)
+            .flatMap(to: Acronym.self, { acronym in
+                return acronym.save(on: req)
+            })
+    }
+    
+    router.get("api", "acronyms") { req -> Future<[Acronym]> in
+        return Acronym
+            .query(on: req)
+            .all()
+    }
+    
+    router.get("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
+        return try req.parameters.next(Acronym.self)
+    }
+    
+    router.put("api", "acronyms", Acronym.parameter) { req -> Future<Acronym>  in
+        return try
+            flatMap(to: Acronym.self,
+                    req.parameters.next(Acronym.self),
+                    req.content.decode(Acronym.self)) { acronym, updateAcronym in
+                        
+                        acronym.short = updateAcronym.short
+                        acronym.long = updateAcronym.long
+                        
+                        return acronym.save(on: req)
+        }
+    }
+    
+    router.delete("api", "acronyms", Acronym.parameter) { req -> Future<HTTPStatus> in
+        return try req.parameters.next(Acronym.self)
+            .delete(on: req)
+            .transform(to: HTTPStatus.noContent)
+    }
+}
